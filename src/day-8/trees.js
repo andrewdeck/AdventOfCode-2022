6a9ed7abb0379842d5d15237edd4d1d43081d032
Andrew Deck <deck.andrew@gmail.com>
2022-12-08 17:36:30 -0500

    day 8 part 2

--- src/day-8/trees.js
+++ src/day-8/trees.js
@@ -23,8 +23,8 @@ function countVisibleTrees(grid) {
 }
 
 function treeIsVisible({x, y, grid}) {
-  let height = grid[y][x];
-  let gridWidth = grid[0].length;
+  const height = grid[y][x];
+  const gridWidth = grid[0].length;
 
   for(let i = x - 1; i >= 0; i--) { //left
     let h = grid[y][i];
@@ -50,4 +50,48 @@ function treeIsVisible({x, y, grid}) {
 
   return false;
 }
-console.log(countVisibleTrees(trees));
+// console.log(countVisibleTrees(trees));
+
+function scenicScore({x, y, grid}) {
+  const height = grid[y][x];
+  const gridWidth = grid[0].length;
+
+  let up = 0, down = 0, left = 0, right = 0;
+
+  for(let i = x - 1; i >= 0; i--) { //left
+    let h = grid[y][i];
+    left++;
+    if(h >= height) break;
+  }
+  for(let i = x + 1; i < gridWidth; i++) { //right
+    let h = grid[y][i];
+    right++;
+    if(h >= height) break;
+  }
+  for(let i = y - 1; i >= 0; i--) { //down
+    let h = grid[i][x];
+    down++;
+    if(h >= height) break;
+  }
+  for(let i = y + 1; i < gridWidth; i++) { //up
+    let h = grid[i][x];
+    up++;
+    if(h >= height) break;
+  }
+  return up * down * left * right;
+}
+
+function findHighestScenicScore(grid) {
+  const firstRow = grid[0];
+  let highestScore = 0;
+
+  for(let x = 1; x < firstRow.length - 1; x ++) {
+    for(let y = 1; y < grid.length - 1; y++) {
+      let score = scenicScore({x, y, grid});
+      if(score > highestScore) highestScore = score;
+    }
+  }
+  return highestScore;
+}
+
+console.log(findHighestScenicScore(trees));
