e04e696cab60a6f3787251c56c753e9b4af15f08
Andrew Deck <deck.andrew@gmail.com>
2022-12-16 21:19:47 -0500

    realizing recursion isnt so simple

---
+++ src/day-13/packets.js
@@ -0,0 +1,75 @@
+import fs from 'fs';
+
+// const inputText = fs.readFileSync('./input.txt', 'utf-8');
+const inputText = fs.readFileSync('./unit.txt', 'utf-8');
+// const inputText = fs.readFileSync('./example.txt', 'utf-8');
+
+const pairs = inputText.split('\n\n');
+
+let score = 0;
+
+pairs.forEach((string, index) => {
+  let [left, right] = string.split('\n').map(JSON.parse);
+  debug();
+  debug(`== Pair ${index + 1} ==`);
+  if(isProperlyOrdered(left, right)) {
+    // debug('valid index: ', index + 1);
+    score += (index + 1);
+  }
+});
+
+console.log(score);
+
+function isProperlyOrdered(left, right, level = 0) {
+  if(right.length === 0) {
+    debug(`- Right side ran out of items, so inputs are not in the right order`, level);
+    return false;
+  }
+  if(left.length === 0) {
+    return true;
+    debug(`Left side ran out of items, so inputs are in the right order`);
+  }
+  debug(`- Compare ${JSON.stringify(left)} vs ${JSON.stringify(right)}`, level);
+
+  for(let i = 0; i < left.length; i++) {
+    let leftItem = left[i],
+        rightItem = right[i];
+    debug(`  - Compare ${JSON.stringify(leftItem)} vs ${JSON.stringify(rightItem)}`, level);
+    const leftType = typeOf(leftItem),
+          rightType = typeOf(rightItem);
+
+    if(leftType === 'undefined') console.log('THIS SHOULD BE TRUE');
+    if(rightType === 'undefined') {
+      debug(`    - Right side ran out of items, so inputs are not in the right order`);
+      return false;
+    } else if(leftType !== rightType) {
+      // debug('one is a list');
+      if(leftType === 'number') leftItem = [leftItem];
+      else rightItem = [rightItem];
+      console.log(`Mixed Types ${JSON.stringify(leftItem)} vs ${JSON.stringify(rightItem)} ${rightType}`)
+      if(!isProperlyOrdered(leftItem, rightItem, level + 1)) return false;
+    } else if( leftType === 'number') {
+      if(leftItem < rightItem) {
+        debug(`    - Left side is smaller, so inputs are in the right order`, level);
+        return true
+      } else if(rightItem < leftItem) {
+        debug(`    - Right side is smaller, so inputs are not in the right order`);
+        return false;
+      }
+    } else { // both lists
+      if(!isProperlyOrdered(leftItem, rightItem, level + 1)) return false;
+    }
+  }
+  return true;
+}
+
+function debug(output = '', level = 0) {
+  let prefix = '';
+  for(let x = 0; x < level; x++) prefix += '  ';
+  console.log(`${prefix}${output}`);
+}
+
+function typeOf(val) {
+  return Array.isArray(val) ? 'array' : typeof val;
+}
+
