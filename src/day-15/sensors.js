17150f58c94eabc52ee8a7f100d2fd2b0840fd23
Andrew Deck <deck.andrew@gmail.com>
2022-12-18 17:30:38 -0500

    day 15 part 2

--- src/day-15/sensors.js
+++ src/day-15/sensors.js
@@ -39,24 +39,60 @@ function manhattanDistance(a, b) {
   return Math.abs(a.x - b.x) + Math.abs(a.y - b.y);
 }
 
-const Y_LEVEL = 2000000;
-let row = {};
-// mark all Y that are in range
-sensors.forEach(sensor => {
-  if(sensor.canReachY(Y_LEVEL)) {
-    const pos = sensor.position;
-    const distance = manhattanDistance(pos, {x: pos.x, y: Y_LEVEL});
-    const reach = sensor.range - distance;
+// const Y_LEVEL = 2000000;
+// let row = {};
+// // mark all Y that are in range
+// sensors.forEach(sensor => {
+//   if(sensor.canReachY(Y_LEVEL)) {
+//     const pos = sensor.position;
+//     const distance = manhattanDistance(pos, {x: pos.x, y: Y_LEVEL});
+//     const reach = sensor.range - distance;
     
-    for(let x = pos.x - reach; x <= pos.x + reach; x++) {
-      row[x] = true;
-    }
+//     for(let x = pos.x - reach; x <= pos.x + reach; x++) {
+//       row[x] = true;
+//     }
+//   }
+// });
+
+// // remove actual beacons
+// beacons.forEach(({x,y}) => {
+//   if(y === Y_LEVEL && row[x]) delete row[x];
+// });
+
+// console.log(Object.keys(row).length);
+
+console.time('part2');
+const MIN = 0, MAX = 4000000;
+// const MIN = 0, MAX = 20;
+sensors.forEach(sensor => {
+  // has to be just outside of sensor range, so look there
+  const pos = sensor.position;
+  
+  let look = { x: pos.x + sensor.range + 1, y: pos.y }; // start on the right, and rotate clockwise
+  while(look.x !== pos.x) {
+    look = {x: look.x-1, y: look.y-1};
+    if(outOfRange(look)) continue;
+    checkPosition(look);
   }
 });
 
-// remove actual beacons
-beacons.forEach(({x,y}) => {
-  if(y === Y_LEVEL && row[x]) delete row[x];
-});
+function outOfRange({x, y}) {
+  return x < MIN || y < MIN || x > MAX || y > MAX;
+}
 
-console.log(Object.keys(row).length);
+function checkPosition({x, y}) {
+  let outOfAllSensorsRange = true;
+  for(let s = 0; s < sensors.length; s++) {
+    let sensor = sensors[s];
+    if(manhattanDistance(sensor.position, {x,y}) <= sensor.range) {
+      outOfAllSensorsRange = false;
+      break; 
+    }
+  }
+  if(outOfAllSensorsRange) {
+    console.log(x,y);
+    console.log( (4000000 * x) + y );
+    console.timeEnd('part2');
+    process.exit();
+  }
+}
