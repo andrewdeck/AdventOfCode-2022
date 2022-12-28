73d2d3b9ef5067dcc24e5782f07f028f69d7c954
Andrew Deck <deck.andrew@gmail.com>
2022-12-28 16:37:28 -0500

    day 16 part 1

--- src/day-16/valves.js
+++ src/day-16/valves.js
@@ -1,8 +1,7 @@
 import fs from 'fs';
-import dijkstra from 'dijkstrajs';
 
-// const inputText = fs.readFileSync('./input.txt', 'utf-8');
-const inputText = fs.readFileSync('./example.txt', 'utf-8');
+const inputText = fs.readFileSync('./input.txt', 'utf-8');
+// const inputText = fs.readFileSync('./example.txt', 'utf-8');
 
 class Valve {
   constructor(id, flowRate, tunnels) {
@@ -12,8 +11,8 @@ class Valve {
   }
 }
 
-let valveMap = {};
-let graph = {};
+let valveMap = new Map();
+let stateMemoMap = new Map();
 
 // Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
 const valves = inputText.split('\n').map(line => {
@@ -21,53 +20,46 @@ const valves = inputText.split('\n').map(line => {
   let [flow, rest] = line.slice(23).split('; ');
   let tunnels = rest.replace('tunnels lead to valves ', '').replace('tunnel leads to valve ', '').split(', ');
   let valve = new Valve(id, Number(flow), tunnels);
-  valveMap[id] = valve;
+  valveMap.set(id, valve);
 
-  let node = {};
-  tunnels.forEach(tun => {
-    node[tun] = 1;
-  });
-  graph[id] = node;
   return valve;
 });
 
-let mostFlow = 0;
-
 const valvesWithFlow = valves.filter(v => v.flowRate);
 
-function exploreOptions(nodeId, openValves = [], priorFlow = 0, minute = 1) {
-  const valve = valveMap[nodeId];
-  const flowForStep = openValves.map(id => valveMap[id].flowRate).reduce((sum, val) => sum + val, 0);
-  const totalFlow = flowForStep + priorFlow;
-  if(minute === 30) {
-    // console.log(totalFlow, mostFlow);
-    if(totalFlow > mostFlow) {
-      mostFlow = totalFlow;
-      console.log(`branch end: ${mostFlow}`);
-    }
-  } else if(openValves.length === valvesWithFlow.length) {
-    // console.log('all valves open');
-    let remainingMins = 30 - (minute + 1);
-    let finalFlow = totalFlow + (remainingMins * flowForStep);
-    // console.log(finalFlow, mostFlow);
-    if(finalFlow > mostFlow) {
-      mostFlow = finalFlow;
-      console.log(`jump end: ${mostFlow}`);
-    }
-  } else {
-    if(valve.flowRate !== 0 && !openValves.includes(nodeId)) {
-      // console.log(`open valve: ${nodeId}`);
-      exploreOptions(nodeId, [...openValves, nodeId], totalFlow, minute + 1);
-    }
-    let closedValves = valvesWithFlow.map(x=> x.id).filter( v => !openValves.includes(v));
-    closedValves.filter(x => x !== nodeId).forEach( closedValve => {
-      let path = dijkstra.find_path(graph, nodeId, closedValve);
-      exploreOptions(path[1], [...openValves], totalFlow, minute + 1);
-    });
+function memoKey(valve, openValves, timeRemaining) {
+  let openStr = [...openValves].sort().join(',');
+  return `${valve}::${openStr}::${timeRemaining}`;
+}
+
+function findMostFlow(valve, openValves, timeRemaining) {
+  if(timeRemaining === 0) return 0;
+
+  const key = memoKey(valve, openValves, timeRemaining);
+  if(stateMemoMap.has(key)) return stateMemoMap.get(key);
+
+  let flow = openValves.reduce((sum, valve) => {
+    return sum + valveMap.get(valve).flowRate;
+  }, 0);
+
+  if(openValves.length === valvesWithFlow.length) { // all valves are open
+    return flow * timeRemaining;
   }
+
+  let answer = 0;
+
+  if (!openValves.includes(valve) && valveMap.get(valve).flowRate) { // open current valve
+    answer = findMostFlow(valve, [...openValves, valve], timeRemaining - 1);
+  } 
+
+  valveMap.get(valve).tunnels.forEach(v => {
+    let optVal = findMostFlow(v, [...openValves], timeRemaining - 1);
+    if(optVal > answer) answer = optVal;
+  });
+
+  stateMemoMap.set(memoKey(valve, openValves, timeRemaining), answer + flow);
+  return answer + flow;
 }
 
 
-
-exploreOptions('AA');
-console.log(mostFlow);
+console.log(findMostFlow('AA', [], 30));
